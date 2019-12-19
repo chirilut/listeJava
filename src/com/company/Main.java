98ac7b5c20051f1153fd467f691327009e71c5eb
chirilut <chirilut_mihai@uahoo.com>
2019-12-19 19:39:47 +0100

    aa

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello.world");
+
+        int x ;
+        x=3;
+        double y=123456d;
+        double sum= x+y;
+        System.out.println(sum);
+
+        }
+
+
+    }
